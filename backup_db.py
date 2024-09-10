a4ff48a5f2ea0856c95cc4b13e751bfb51284ca2
Dimitris Kagialis <kagialisd@gmail.com>
2024-09-10 23:32:48 +0300

    Refactor backup script to use dynamic environment variables
    Replaced hardcoded values with environment variables for better flexibility. Improved logging and error handling for SSH client creation and database backups. Refactored functions to be more modular and reusable.

--- backup_db.py
+++ backup_db.py
@@ -11,11 +11,18 @@ FILE_PATTERN_ALL = "~/esc_all_system_dbs_{timestamp}.{ext}"
 FILE_PATTERN_DB = "~/esc_db_{timestamp}.{ext}"
 
 
-def create_ssh_client(hostname, port, username, key_filename):
+def get_env_variable(name):
+    value = os.getenv(name)
+    if not value:
+        raise ValueError(f"Missing environment variable '{name}'")
+    return value
+
+
+def create_ssh_client(ssh_host, ssh_port, ssh_user, ssh_key_path):
     client = paramiko.SSHClient()
     client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname, port=port, username=username, key_filename=key_filename)
+    client.set_missing_host_key_policy(paramiko.WarningPolicy())
+    client.connect(ssh_host, port=ssh_port, username=ssh_user, key_filename=ssh_key_path)
     logging.info('SSH Client created')
     return client
 
@@ -28,25 +35,37 @@ def generate_backup_files(timestamp):
     ]
 
 
-def generate_backup_commands(timestamp):
+def generate_backup_command(postgres_user, command, filename):
+    return f'sudo su {postgres_user} -c "{command} {filename}"'
+
+
+def generate_backup_commands(timestamp, postgres_user, dbname):
     backup_files = generate_backup_files(timestamp)
+    commands = {
+        "dumpall": f"pg_dumpall -U {postgres_user} -f",
+        "dump": f"pg_dump -d {dbname} -f",
+        "dump_tar": f"pg_dump -d {dbname}  -F tar"
+    }
+
     return [
-        f'sudo su postgres -c "pg_dumpall -U postgres -f {backup_files[0]}"',
-        f'sudo su postgres -c "pg_dump -d esc -f {backup_files[1]}"',
-        f'sudo su postgres -c "pg_dump -d esc -F tar -f {backup_files[2]}"'
+        generate_backup_command(postgres_user, commands['dumpall'], backup_files[0]),
+        generate_backup_command(postgres_user, commands['dump'], backup_files[1]),
+        generate_backup_command(postgres_user, commands['dump_tar'], backup_files[2])
     ]
 
 
-def create_database_backups(ssh_client, timestamp):
+def create_database_backups(ssh_client, timestamp, postgres_user, dbname):
     logging.info('Creating database backups')
-    commands = generate_backup_commands(timestamp)
+    commands = generate_backup_commands(timestamp, postgres_user, dbname)
     for command in commands:
         logging.info(f'Creating database backup with command: {command}')
         ssh_stdin, ssh_stdout, ssh_stderr = ssh_client.exec_command(command)
-        if ssh_stderr:
+        exit_status = ssh_stdout.channel.recv_exit_status()
+        if exit_status != 0:
             logging.error(f'Creating database backup failed: {ssh_stderr.read()}')
         else:
-            logging.error(f'Creating database backup completed: {ssh_stdout.read()}')
+            logging.info(f'Creating database backup completed: {ssh_stdout.read()}')
+
 
 def download_database_backups(ssh_client, timestamp):
     logging.info('Collecting backup files')
@@ -59,19 +78,21 @@ def download_database_backups(ssh_client, timestamp):
 
 
 def main():
+    logging.info('Backup starts')
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     load_dotenv()
-    ssh_host = os.getenv("SSH_HOST")
-    ssh_port = int(os.getenv("SSH_PORT"))
-    ssh_user = os.getenv("SSH_USER")
-    ssh_key_path = os.getenv("SSH_KEY_PATH")
+    ssh_host = get_env_variable("SSH_HOST")
+    ssh_port = int(get_env_variable("SSH_PORT"))
+    ssh_user = get_env_variable("SSH_USER")
+    ssh_key_path = get_env_variable("SSH_KEY_PATH")
+    postgres_user = get_env_variable("POSTGRES_USER")
+    dbname = get_env_variable("DBNAME")
     ssh_client = create_ssh_client(ssh_host, ssh_port, ssh_user, ssh_key_path)
-    create_database_backups(ssh_client, timestamp)
+    create_database_backups(ssh_client, timestamp, postgres_user, dbname)
     download_database_backups(ssh_client, timestamp)
     ssh_client.close()
+    logging.info('Backup completed successfully')
 
 
 if __name__ == '__main__':
-    logging.info('Backup starts')
     main()
-    logging.info('Backup completed successfully')
