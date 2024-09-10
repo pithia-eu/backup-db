aba435f4116bc0bf93702614e6e401677a0d63fb
Dimitris Kagialis <kagialisd@gmail.com>
2024-09-11 01:26:47 +0300

    Refactor setup script to improve flexibility and error handling
    Updated the script to dynamically determine the Python version and navigate to the project directory. Added checks for directory existence and navigability, as well as improved the venv creation and package installation processes. This enhances the script’s adaptability and robustness.

--- bin/setup_venv.sh
+++ bin/setup_venv.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-# set python version, venv directory and requirements file
-PYTHON_VERSION=3.10
+# Name of the venv
 VENV_NAME=venv
+
+# Python version
+PYTHON_VERSION=python3
+echo "Python version is: $PYTHON_VERSION"
+
+# Project directory
+DIR="/home/ubuntu/backup-db"
+
+# Requirements file
 REQUIREMENTS=requirements.txt
+
+# Check if the directory exists
+if [ ! -d "$DIR" ]; then
+  echo "Directory $DIR does not exist. Stopping execution."
+  exit 1
+fi
+
+# Navigate to project directory
+cd "$DIR" || {
+  echo "Cannot navigate to directory $DIR. Stopping execution."
+  exit 1
+}
+
 
 # check if requirements file exists
 if [ ! -f "$REQUIREMENTS" ]; then
@@ -13,16 +34,16 @@ fi
 
 # create a new venv if it doesn't exist
 if [ ! -d "$VENV_NAME" ]; then
-    python$PYTHON_VERSION -m venv $VENV_NAME
+    $PYTHON_VERSION -m venv $VENV_NAME
 fi
 
 # activate the venv
 source $VENV_NAME/bin/activate
 
 # Update pip to the latest version
-python -m pip install --upgrade pip
+$PYTHON_VERSION -m pip install --upgrade pip
 
 # update the requirements
-pip3 install -r $REQUIREMENTS --upgrade
+$PYTHON_VERSION -m pip install -r $REQUIREMENTS --upgrade
 
 echo "Setup (or update) completed."
