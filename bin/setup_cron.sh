973a50bfceba61fab01df35c33d263ca18f3b193
Dimitris Kagialis <kagialisd@gmail.com>
2024-09-11 01:18:20 +0300

    Add dependency checks and enhance safety in cron setup
    Added checks for essential commands to ensure script dependencies are met. Unified error handling with `set -euo pipefail` and modified script to guarantee backup script and log are executable and writable. Simplified cron job check and creation logic for improved readability and reliability.

--- bin/setup_cron.sh
+++ bin/setup_cron.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ # This will make script fail on first error
+set -euo pipefail
+
 # Project directory
 DIR="/home/ubuntu/backup-db"
 
@@ -9,13 +12,23 @@ SCRIPT_PATH="$DIR/bin/backup_db.sh"
 # Path to the log file
 LOGFILE_PATH="$DIR/log/backup_db.log"
 
-# Fail fast if crontab is not installed
+# List of commands which our script is dependent on
+declare -a dependencies=("crontab" "mkdir" "touch" "pgrep" "chmod")
+
+# Check for dependencies
+for cmd in "${dependencies[@]}"
+do
+  if ! command -v "$cmd" >/dev/null 2>&1; then
+    echo "$cmd is not installed or not in PATH" >&2
+    exit 1
+  fi
+done
+
+# Crontab and Cron daemon checking
 if ! command -v crontab >/dev/null 2>&1; then
   echo "Crontab is not installed or not in PATH" >&2
   exit 1
 fi
-
-# Check for cron daemon running
 if ! pgrep cron >/dev/null 2>&1; then
   echo "Cron daemon is not running" >&2
   exit 1
@@ -25,8 +38,8 @@ fi
 for directory in "$DIR" "${SCRIPT_PATH%/*}" "${LOGFILE_PATH%/*}"; do
   if [ ! -d "$directory" ]; then
     echo "$directory does not exist. Creating now..."
-    mkdir -p "$directory"
   fi
+  mkdir -p "$directory"
 done
 
 # Check if the backup script file exists
@@ -35,28 +48,24 @@ if [ ! -f "$SCRIPT_PATH" ]; then
   exit 1
 fi
 
-# Check if the backup script is executable
-if [ ! -x "$SCRIPT_PATH" ]; then
-  echo "Backup script at $SCRIPT_PATH is not executable" >&2
-  chmod +x "$SCRIPT_PATH"
-fi
+# Always set the backup script as executable
+echo "Ensure backup script at $SCRIPT_PATH is executable"
+chmod +x "$SCRIPT_PATH"
 
 # Check if the log file exists, if not create it
 if [ ! -f "$LOGFILE_PATH" ]; then
   echo "Log file not found at $LOGFILE_PATH, creating now..."
-  touch "$LOGFILE_PATH"
 fi
+touch "$LOGFILE_PATH"
 
-# Check if the log file is writable
-if [ ! -w "$LOGFILE_PATH" ]; then
-  echo "Log file at $LOGFILE_PATH is not writable, changing permissions now..."
-  chmod +w "$LOGFILE_PATH"
-fi
+# Always set the log file as writable
+echo "Ensure log file at $LOGFILE_PATH is writable"
+chmod +w "$LOGFILE_PATH"
 
 # Check for existing cron job
-if crontab -l 2>/dev/null | grep -q "$SCRIPT_PATH >> $LOGFILE_PATH 2>&1"; then
-    echo "Cron job already exists"
-else
-    echo "Adding cron job"
-    (crontab -l 2>/dev/null ; echo "0 0 * * * $SCRIPT_PATH >> $LOGFILE_PATH 2>&1") | crontab -
+croncmd="$SCRIPT_PATH >> $LOGFILE_PATH 2>&1"
+cronjob="0 0 * * * $croncmd"
+if ! (crontab -l 2>/dev/null | grep -Fq "$croncmd"); then
+  echo "Adding cron job"
+  (crontab -l 2>/dev/null ; echo "$cronjob") | crontab -
 fi
