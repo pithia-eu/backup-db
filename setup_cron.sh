b05fe3bdbe9e08f01d3ea638c1cdf33bf245c7a2
Dimitris Kagialis <kagialisd@gmail.com>
2024-09-10 23:49:05 +0300

    Add setup_cron.sh to configure backup database cron job
    A new script setup_cron.sh is introduced to automate adding a cron job for backing up the database. The cron job runs daily at midnight and logs outputs to a specified logfile.

---
+++ setup_cron.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Path to the script
+SCRIPT_PATH="/home/ubuntu/backup-db/backup_db.sh"
+
+# Path to the log file
+LOGFILE_PATH="/home/ubuntu/backup-db/backup_db.log"
+
+# Add the cron job
+(crontab -l ; echo "0 0 * * * $SCRIPT_PATH >> $LOGFILE_PATH 2>&1") | crontab -
