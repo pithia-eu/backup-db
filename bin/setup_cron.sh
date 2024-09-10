8b6fd29eb8a9385230e49e604d2917d4dd7ebaf1
Dimitris Kagialis <kagialisd@gmail.com>
2024-09-11 00:36:49 +0300

    Refactor: Move setup_venv.sh to bin directory
    Relocated setup_venv.sh script to the bin directory for better organization. This change helps in maintaining a cleaner project structure and improves script discoverability.

--- bin/setup_cron.sh
+++ bin/setup_cron.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Path to the script
-SCRIPT_PATH="/home/ubuntu/backup-db/backup_db.sh"
+SCRIPT_PATH="/home/ubuntu/backup-db/bin/backup_db.sh"
 
 # Path to the log file
-LOGFILE_PATH="/home/ubuntu/backup-db/backup_db.log"
+LOGFILE_PATH="/home/ubuntu/backup-db/log/backup_db.log"
 
 # Add the cron job
 (crontab -l ; echo "0 0 * * * $SCRIPT_PATH >> $LOGFILE_PATH 2>&1") | crontab -
