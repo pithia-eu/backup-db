880440ce4c7a61b1d641fe362bacb05bbf11b145
Dimitris Kagialis <kagialisd@gmail.com>
2024-09-11 18:37:29 +0300

    Refactor logger setup to improve modularity.
    Splits the logger setup into distinct functions `_setup_logger`, `_create_console_handler`, and `_create_file_handler` for better modularity and clarity. Removes the `verbose` parameter and error logging now exits the program immediately.

--- source/log/logger.py
+++ source/log/logger.py
@@ -2,7 +2,7 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-DEFAULT_LOG_NAME = "backup-db"
+DEFAULT_LOG_NAME = "backup"
 DEFAULT_LOG_PATH = "log/main.log"
 DEFAULT_LOG_LEVEL = logging.DEBUG
 DEFAULT_LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -10,53 +10,53 @@ DEFAULT_MAX_BYTES = 100000
 DEFAULT_BACKUP_COUNT = 5
 
 
-def setup_logger(log_name=DEFAULT_LOG_NAME,
-                 file_path=DEFAULT_LOG_PATH,
-                 log_level=DEFAULT_LOG_LEVEL,
-                 log_format=DEFAULT_LOG_FORMAT,
-                 max_bytes=DEFAULT_MAX_BYTES,
-                 backup_count=DEFAULT_BACKUP_COUNT,
-                 verbose=True):
+def _setup_logger(log_name=DEFAULT_LOG_NAME,
+                  file_path=DEFAULT_LOG_PATH,
+                  log_level=DEFAULT_LOG_LEVEL,
+                  log_format=DEFAULT_LOG_FORMAT,
+                  max_bytes=DEFAULT_MAX_BYTES,
+                  backup_count=DEFAULT_BACKUP_COUNT):
+    _logger = logging.getLogger(log_name)
+    _logger.setLevel(log_level)
+    c_handler = _create_console_handler(log_format, log_level)
+    _logger.addHandler(c_handler)
+    f_handler = _create_file_handler(_logger, backup_count, file_path, log_format, log_level, max_bytes)
+    _logger.addHandler(f_handler)
+    return _logger
 
-    # Create a logger
-    logger = logging.getLogger(log_name)  # Give logger a specific name
-    logger.setLevel(log_level)  # Sets the threshold for this logger to level.
 
-    # Check if the directory exists and script has write access to the log file directory
+def _create_console_handler(log_format, log_level):
+    c_handler = logging.StreamHandler()
+    c_handler.setLevel(log_level)
+    c_formatter = logging.Formatter(log_format)
+    c_handler.setFormatter(c_formatter)
+    return c_handler
+
+
+def _create_file_handler(_logger, backup_count, file_path, log_format, log_level, max_bytes):
     directory = os.path.dirname(os.path.abspath(file_path))
     if not os.path.exists(directory):
-        logger.error(f"Directory {directory} does not exist.")
+        _logger.error(f"Directory {directory} does not exist.")
+        _logger.info(f"Program exits with error")
         raise FileNotFoundError(f"Directory {directory} does not exist.")
     if not os.access(directory, os.W_OK):
-        logger.error(f"Script does not have write access to the directory {directory}.")
+        _logger.error(f"Script does not have write access to the directory {directory}.")
+        _logger.info(f"Program exits with error")
         raise PermissionError(f"Script does not have write access to the directory {directory}.")
-
-    # Create handlers
-    # Console handler (only add if verbose is True)
-    if verbose:
-        c_handler = logging.StreamHandler()
-        c_handler.setLevel(log_level)  # Console handler level
-        # Create formatters and add to console handler
-        c_formatter = logging.Formatter(log_format)
-        c_handler.setFormatter(c_formatter)
-        # Add console handler to the logger
-        logger.addHandler(c_handler)
-
-    # Rotating File Handler
     try:
         f_handler = RotatingFileHandler(file_path, maxBytes=max_bytes, backupCount=backup_count)
     except PermissionError:
-        logger.exception("There is no write permission to create a log file at this path.", exc_info=True)
+        _logger.exception("There is no write permission to create a log file at this path.", exc_info=True)
+        _logger.info(f"Program exits with error")
         raise
     except FileNotFoundError:
-        logger.exception("The provided file path does not exist.", exc_info=True)
+        _logger.exception("The provided file path does not exist.", exc_info=True)
+        _logger.info(f"Program exits with error")
         raise
-
-    f_handler.setLevel(log_level)  # File handler level
-    # Create formatters and add them to file handler
+    f_handler.setLevel(log_level)
     f_formatter = logging.Formatter(log_format)
     f_handler.setFormatter(f_formatter)
-    # Add file handler to the logger
-    logger.addHandler(f_handler)
+    return f_handler
 
-    return logger
+
+logger = _setup_logger()
