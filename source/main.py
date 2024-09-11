ed155f3f0941e6fb72a5e8ca5423e539822a11a4
Dimitris Kagialis <kagialisd@gmail.com>
2024-09-11 18:25:47 +0300

    Refactor logger setup in main script
    Replaced explicit logger setup with an imported logger instance. Simplified spacing and removed redundant lines for better readability and maintainability.

--- source/main.py
+++ source/main.py
@@ -1,40 +1,30 @@
 from datetime import datetime
 from dotenv import load_dotenv
 
-from source.log.logger import setup_logger
+from source.log.logger import logger
 from source.utils.arguments import process_args
 from source.utils.environment import get_env_variable
 from source.backup.backup import backup
 from source.restore.restore import restore
 
-logger = setup_logger()
-
 
 def main():
-
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-
     args = process_args()
-
     test = False
-
     if args.test:
         test = True
         logger.warning("Test mode in enabled. No backups will be created")
     else:
         logger.info('Database backup starts')
-
     load_dotenv()
-
     backup(timestamp, test)
-
     if get_env_variable("RESTORE"):
         restore(test)
-
     if args.test:
         logger.warning("Test run completed. No backups created")
     else:
-       logger.info('Backup completed successfully')
+        logger.info('Backup completed successfully')
 
 
 if __name__ == '__main__':
