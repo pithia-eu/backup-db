84f320ba3956eb05b3545ebd3f485a3f258e4ab7
Dimitris Kagialis <kagialisd@gmail.com>
2024-09-11 18:55:21 +0300

    Enhance SSH client creation with error handling
    Added checks for SSH key file existence and incorporated detailed exception handling for various SSH errors. The code now logs specific errors and raises exceptions appropriately, improving error traceability and robustness.

--- source/utils/ssh.py
+++ source/utils/ssh.py
@@ -1,12 +1,35 @@
+import os
 import paramiko
-
 from source.log.logger import logger
 
 
 def create_ssh_client(ssh_host, ssh_port, ssh_user, ssh_key_path):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.WarningPolicy())
-    client.connect(ssh_host, port=ssh_port, username=ssh_user, key_filename=ssh_key_path)
-    logger.info('SSH Client created')
-    return client
+    if not os.path.exists(ssh_key_path) or not os.path.isfile(ssh_key_path):
+        logger.error(f'SSH key file does not exist at the given path: {ssh_key_path}')
+        logger.info(f"Program exits with error")
+        raise FileNotFoundError(f'SSH key file does not exist at the given path: {ssh_key_path}')
+    try:
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.WarningPolicy())
+        client.connect(ssh_host, port=ssh_port, username=ssh_user, key_filename=ssh_key_path)
+        logger.info('SSH client created')
+        logger.debug(f'host:{ssh_host} port:{ssh_port} user:{ssh_user}')
+        return client
+    except paramiko.AuthenticationException:
+        logger.exception("Authentication failed, please verify your credentials.", exc_info=True)
+        logger.info(f"Program exits with error")
+        raise
+    except paramiko.SSHException as e:
+        logger.exception("Unable to establish SSH connection: {}".format(e), exc_info=True)
+        logger.info(f"Program exits with error")
+        raise
+    except paramiko.ssh_exception.NoValidConnectionsError as e:
+        logger.error("No valid connections could be established: {}".format(e))
+        logger.info(f"Program exits with error")
+        raise
+    except Exception as e:
+        logger.exception("Unexpected error: {}".format(e), exc_info=True)
+        logger.info(f"Program exits with error")
+        raise
+
