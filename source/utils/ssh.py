2f18772d2a731cf3455c5e15ddb9f12793f972bc
Dimitris Kagialis <kagialisd@gmail.com>
2024-09-12 10:50:55 +0300

    Refactor create_ssh_client for better readability
    Refactored the create_ssh_client function to improve code readability by adding line breaks for long lines. This enhances maintainability and ensures consistent formatting across the file.

--- source/utils/ssh.py
+++ source/utils/ssh.py
@@ -3,7 +3,10 @@ import paramiko
 from source.log.logger import logger
 
 
-def create_ssh_client(ssh_host, ssh_port, ssh_user, ssh_key_path):
+def create_ssh_client(ssh_host,
+                      ssh_port,
+                      ssh_user,
+                      ssh_key_path):
     if not os.path.exists(ssh_key_path) or not os.path.isfile(ssh_key_path):
         logger.error(f'SSH key file does not exist at the given path: {ssh_key_path}')
         logger.info(f"Program exits with error")
@@ -12,15 +15,20 @@ def create_ssh_client(ssh_host, ssh_port, ssh_user, ssh_key_path):
         client = paramiko.SSHClient()
         client.load_system_host_keys()
         client.set_missing_host_key_policy(paramiko.WarningPolicy())
-        client.connect(ssh_host, port=ssh_port, username=ssh_user, key_filename=ssh_key_path)
+        client.connect(ssh_host,
+                       port=ssh_port,
+                       username=ssh_user,
+                       key_filename=ssh_key_path)
         logger.info('SSH client created')
         return client
     except paramiko.AuthenticationException:
-        logger.exception("Authentication failed, please verify your credentials.", exc_info=True)
+        logger.exception("Authentication failed, please verify your credentials.",
+                         exc_info=True)
         logger.info(f"Program exits with error")
         raise
     except paramiko.SSHException as e:
-        logger.exception("Unable to establish SSH connection: {}".format(e), exc_info=True)
+        logger.exception("Unable to establish SSH connection: {}".format(e),
+                         exc_info=True)
         logger.info(f"Program exits with error")
         raise
     except paramiko.ssh_exception.NoValidConnectionsError as e:
@@ -28,7 +36,8 @@ def create_ssh_client(ssh_host, ssh_port, ssh_user, ssh_key_path):
         logger.info(f"Program exits with error")
         raise
     except Exception as e:
-        logger.exception("Unexpected error: {}".format(e), exc_info=True)
+        logger.exception("Unexpected error: {}".format(e),
+                         exc_info=True)
         logger.info(f"Program exits with error")
         raise
 
