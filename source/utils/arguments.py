d0735045a0b765286fbb04460ac40cce31262e3d
Dimitris Kagialis <kagialisd@gmail.com>
2024-09-12 10:51:08 +0300

    Refactor argument addition in process_args
    Reformat the '--test' argument addition in process_args to improve readability and maintain consistency with multi-line argument definitions. This update makes the code more maintainable and easier to read.

--- source/utils/arguments.py
+++ source/utils/arguments.py
@@ -3,6 +3,8 @@ import argparse
 
 def process_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--test', action='store_true', help='Enable test mode')
+    parser.add_argument('--test',
+                        action='store_true',
+                        help='Enable test mode')
     args = parser.parse_args()
     return args
