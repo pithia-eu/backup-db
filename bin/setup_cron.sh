fac36894f2170b506c6cd33da3c35d670159a325
Dimitris Kagialis <kagialisd@gmail.com>
2024-09-12 23:14:20 +0300

    Add status messages to setup_cron.sh
    Introduce echo statements to provide feedback during script execution. These messages help clarify each step of the setup process, including dependency checks, directory creation, and the status of the cron daemon and cron job.

--- bin/setup_cron.sh
+++ bin/setup_cron.sh
@@ -16,6 +16,7 @@ LOGFILE_PATH="$DIR/log/backup_db.log"
 declare -a dependencies=("crontab" "mkdir" "touch" "pgrep" "chmod")
 
 # Check for dependencies
+echo "Checking if all dependencies are installed"
 for cmd in "${dependencies[@]}"
 do
   if ! command -v "$cmd" >/dev/null 2>&1; then
@@ -25,12 +26,14 @@ do
 done
 
 # Cron daemon checking
+echo "Checking if cron daemon runs"
 if ! pgrep cron >/dev/null 2>&1; then
   echo "Cron daemon is not running" >&2
   exit 1
 fi
 
 # Check if the directories exist and create them if they do not
+echo "Checking if required directories exist"
 for directory in "$DIR" "${SCRIPT_PATH%/*}" "${LOGFILE_PATH%/*}"; do
   if [ ! -d "$directory" ]; then
     echo "$directory does not exist. Creating now..."
@@ -43,13 +46,14 @@ for directory in "$DIR" "${SCRIPT_PATH%/*}" "${LOGFILE_PATH%/*}"; do
 done
 
 # Check if the backup script file exists
+echo "Checking if the backup script exist"
 if [ ! -f "$SCRIPT_PATH" ]; then
   echo "Backup script not found at $SCRIPT_PATH" >&2
   exit 1
 fi
 
 # Always set the backup script as executable
-echo "Ensure backup script at $SCRIPT_PATH is executable"
+echo "Checking if the backup script at $SCRIPT_PATH is executable"
 chmod +x "$SCRIPT_PATH"
 if [ $? -ne 0 ]; then
   echo "Failed to set backup script as executable." >&2
@@ -57,6 +61,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Check if the log file exists, if not create it
+echo "Checking if the the log file exist"
 if [ ! -f "$LOGFILE_PATH" ]; then
   echo "Log file not found at $LOGFILE_PATH, creating now..."
   touch "$LOGFILE_PATH"
@@ -67,7 +72,7 @@ if [ ! -f "$LOGFILE_PATH" ]; then
 fi
 
 # Always set the log file as writable
-echo "Ensure log file at $LOGFILE_PATH is writable"
+echo "Checking if the log file at $LOGFILE_PATH is writable"
 chmod +w "$LOGFILE_PATH"
 if [ $? -ne 0 ]; then
   echo "Failed to set log file as writable." >&2
@@ -75,6 +80,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Check for existing cron job
+echo "Checking if the  cron job exist"
 croncmd="$SCRIPT_PATH >> $LOGFILE_PATH 2>&1"
 cronjob="0 0 * * * $croncmd"
 if ! (crontab -l 2>/dev/null | grep -Fq "$croncmd"); then
@@ -85,3 +91,5 @@ if ! (crontab -l 2>/dev/null | grep -Fq "$croncmd"); then
     exit 1
   fi
 fi
+
+echo "Backuo cron job setup completed"
